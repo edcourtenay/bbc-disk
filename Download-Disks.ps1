2870252de7155e1242e165640763e06dbf34d3e3
Ed Courtenay <ed@edcourtenay.co.uk>
2024-01-17 22:40:12 +0000

    Add BBC Disk downloading script and related files
    Included a PowerShell script (Download-Disks.ps1) that downloads specific BBC disks for emulation. This commit also adds associated .idea files for BBCDisk and updates .gitignore to ignore the "disks/" directory holding the downloaded files.

---
+++ Download-Disks.ps1
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env -S pwsh -NoProfile
+
+$disks = @{
+    "2088/Disc999-EliteMasterAndTubeEnhanced.ssd" = "elite.ssd";
+    "25/Disc002-ChuckieEgg.ssd" = "chuckie-egg.ssd"
+};
+
+New-Item ./disks -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+
+foreach($key in $disks.Keys) {
+    $url = "https://bbcmicro.co.uk/gameimg/discs/$key"
+    $file = "disks/$($disks[$key])"
+    Write-Host "Downloading $url to $file"
+    Invoke-WebRequest -Uri $url -OutFile $file
+}
