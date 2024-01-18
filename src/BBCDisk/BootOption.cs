f90a30c593b11a655fa9573a257d0c5235a76520
Ed Courtenay <ed@edcourtenay.co.uk>
2024-01-18 13:24:09 +0000

    Initial catalog entry reading
    Added functionality for interpreting boot options and reading catalog entries via newly created BootOption enumerator and CatalogEntry class.

---
+++ src/BBCDisk/BootOption.cs
@@ -0,0 +1,9 @@
+﻿namespace BBCDisk;
+
+public enum BootOption
+{
+    None = 0b00,
+    Load = 0b01,
+    Run =  0b10,
+    Exec = 0b11
+}
---
+++ src/BBCDisk/CatalogEntry.cs
@@ -0,0 +1,30 @@
+﻿namespace BBCDisk;
+
+public record CatalogEntry
+{
+    private readonly int _loadAddress;
+    private readonly int _execAddress;
+    public required string Filename { get; init; }
+    public required char Directory { get; init; } = '$';
+    public required bool Locked { get; init; }
+
+    public required int LoadAddress
+    {
+        get => _loadAddress;
+        init => _loadAddress = Address(value);
+    }
+
+    public required int ExecAddress
+    {
+        get => _execAddress;
+        init => _execAddress = Address(value);
+    }
+
+    public required int Length { get; init; }
+    public required int StartSector { get; init; }
+
+    private int Address(int value) => (value & 0x030000) == 0x030000 ? value | 0xFF0000 : value;
+
+    public override string ToString() =>
+        $"{Directory}.{Filename,-7} {(Locked ? 'L' : ' ')} {LoadAddress:X6} {ExecAddress:X6} {Length:X6} {StartSector:X3}";
+}
